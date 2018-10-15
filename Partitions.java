5c20386d65f73d818b6faaede388f2ce30f63d65
lequytra <lequytra@grinnell.edu>
2018-10-15 17:05:36 -0500

    Second Commit

--- Partitions.java
+++ Partitions.java
@@ -9,13 +9,13 @@ public class Partitions {
 		int left = 1, right = len - 1; 
 		int pivot = nums[0]; 
 		while (left <= right) {
-			while (nums[left] <= pivot) {
+			while (left <= right && nums[left] <= pivot) {
 				left++; 
 			}
 			while (nums[right] > pivot) {
 				right--; 
 			}
-			if (left <= right) {
+			if (left < right) {
 				int temp = nums[right]; 
 				nums[right] = nums[left]; 
 				nums[left] = temp; 
@@ -29,20 +29,52 @@ public class Partitions {
 	
 	public static int select (int a[], int n, int k) {
 		int mid = partition(a); 
-		if (k == mid) return mid; 
-		if (k < mid) return select(Arrays.copyOfRange(a, 0, mid), mid, k); 
-		if (k > mid) return select(Arrays.copyOfRange(a, mid, n - mid), mid, k - (mid -1));
-		return mid;
+		if (k > n || k == 0) {
+			throw new IndexOutOfBoundsException("k cannot be larger than array's size or equal to 0."); 
+		}
+		if (k == mid + 1) return a[mid]; 
+		if (k < mid + 1) {
+			int [] arr = Arrays.copyOfRange(a, 0, mid);
+			return select(arr, mid, k); 
+		}
+		if (k > mid + 1) {
+			int [] arr = Arrays.copyOfRange(a, mid + 1, n); 
+			return select(arr, n - mid - 1, k - mid - 1);
+		}
+		return a[mid];
+	}
+	
+	public static int median (int a[], int n) {
+		int mid = (n%2 == 0)? n/2 : (n+1)/2;
+		return select(a, n, mid); 
+	}
+	
+	public static void quickSort(int a[], int n) {
+		if(n > 1) {
+			int mid = partition(a); 
+			int [] arrRight = Arrays.copyOfRange(a, mid + 1, n);
+			quickSort(arrRight, n - mid - 1); 
+			int [] arrLeft = Arrays.copyOfRange(a, 0, mid - 1);
+			quickSort(arrLeft, mid - 1); 
+		}
 	}
 	
 	public static void main (String [] args) {
 		int [] arr = {1, 2, 3, -5, 8, -3, 7}; 
+		int [] arr3 = {100, 50, 30, 20, 10}; 
+		int [] arr4 = {1, 2, 3 , 4, 5, 6}; 
 		int [] arr2 = {5, 8, -1, 4, 10, 2}; 
 		int len = arr.length;
 		int len2 = arr2.length;
+		/**
 		System.out.println(partition(arr)); 
 		System.out.println(partition(arr2)); 
-		System.out.println(select(arr, 7, 4));
+		System.out.println(select(arr, 7, 1));
+		System.out.println(select(arr2, 6, 5));
+		System.out.println(median(arr, 7));
+		System.out.println(median(arr2, 6));
+		*/
+		quickSort(arr2, len2); 
 		System.out.println(); 
 		for (int i = 0; i < len; i++) {
 			System.out.println(arr[i]);
