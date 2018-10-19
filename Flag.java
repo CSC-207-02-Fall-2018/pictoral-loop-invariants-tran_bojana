becf81126d0cba239390cc471a6e0003842dbb3c
lequytra <lequytra@grinnell.edu>
2018-10-18 22:15:09 -0500

    Javadocs and test cases added

--- Flag.java
+++ Flag.java
@@ -1,77 +1,205 @@
 package partitions;
 
+/**
+ * The Flag class provide methods sorting an array of colors in the order of the 
+ * Dutch national flag - red, white, and blue. 
+ * An instance of Flag has no fields. However, it supports declaring new type 
+ * color which can be either red, white, or blue. 
+ */
 public class Flag {
+	
+	//define a new type color which can be either red, white, or blue
 	enum color {
 		red, white, blue
 	}; 
-	
+
+	/**
+	 * Pre-conditions: none
+	 * Post-conditions: The array of colors is printed
+	 * @param an array of elements type color
+	 * @return nothing
+	 * */
 	public void printColor(color [] colors) {
 		for (color some_color : colors) {
-			System.out.println(some_color);
+			System.out.println(some_color); //prints the colors in the array one by one
 		}
 		System.out.println(); 
 	}
-	
-	public void InvariantA(color [] colors) {
+
+	/**
+	 * Method rearranges the input array of elements in order of the Dutch national flag. 
+	 * Pre-conditions: none
+	 * Post-conditions: The array of colors is sorted in the order of the Dutch flag
+	 * @param an array of elements type color
+	 * @return nothing
+	 * */
+	public void InvariantA (color [] colors) {
 		int r_spot = -1, w_spot = -1, b_spot = -1;  
 		for (color some_color : colors) {
 			switch(some_color) {
-				case red: 
-					r_spot++; w_spot++; b_spot++; 
-					color temp = colors[r_spot]; 
-					colors[r_spot] = colors[b_spot]; 
-					colors[b_spot] = colors[w_spot]; 
-					colors[w_spot] = temp; 
-					break; 
-				case white: 
-					w_spot++; b_spot++; 
-					color temp_w = colors[w_spot]; 
-					colors[w_spot] = colors[b_spot]; 
-					colors[b_spot] = temp_w; 
-					break; 
-				case blue: 
-					b_spot++; 
-					break; 
+			case red: 
+				r_spot++; 
+				w_spot++; 
+				b_spot++; 
+				color temp = colors[r_spot]; 
+				colors[r_spot] = colors[b_spot]; 
+				colors[b_spot] = colors[w_spot]; 
+				colors[w_spot] = temp; 
+				break; 
+			case white: 
+				w_spot++; 
+				b_spot++; 
+				color temp_w = colors[w_spot]; 
+				colors[w_spot] = colors[b_spot]; 
+				colors[b_spot] = temp_w; 
+				break; 
+			case blue: 
+				b_spot++; 
+				break; 
 			}
-			
 		}
 	}
-	
-	public void InvariantD(color [] colors) {
+
+	/**
+	 * Method rearranges the input array of elements in order of the Dutch national flag. 
+	 * Pre-conditions: none
+	 * Post-conditions: The array of colors is sorted in the order of the Dutch flag
+	 * @param an array of elements type color
+	 * @return nothing
+	 * */
+	public void InvariantD (color [] colors) {
 		int size_color = colors.length; 
 		int w_spot = size_color, b_spot = size_color, r_spot = size_color; 
 		for (int i = size_color - 1; i >= 0; i--) {
 			switch(colors[i]) {
-				case blue: 
-					b_spot--; w_spot--; r_spot--; 
-					color temp = colors[r_spot]; 
-					colors[r_spot] = colors[w_spot]; 
-					colors[w_spot] = colors[b_spot]; 
-					colors[b_spot] = temp; 
-					break; 
-				case white: 
-					w_spot--; r_spot--; 
-					color temp_w = colors[r_spot]; 
-					colors[r_spot] = colors[w_spot]; 
-					colors[w_spot] = temp_w; 
-					break; 
-				case red: 
-					r_spot--; 
-					break; 
+			case blue: 
+				b_spot--; 
+				w_spot--; 
+				r_spot--; 
+				color temp = colors[r_spot]; 
+				colors[r_spot] = colors[w_spot]; 
+				colors[w_spot] = colors[b_spot]; 
+				colors[b_spot] = temp; 
+				break; 
+			case white: 
+				w_spot--; 
+				r_spot--; 
+				color temp_w = colors[r_spot]; 
+				colors[r_spot] = colors[w_spot]; 
+				colors[w_spot] = temp_w; 
+				break; 
+			case red: 
+				r_spot--; 
+				break; 
 			}
 		}
-		for (color some_color2 : colors) {
-			System.out.println(some_color2);
-		}
-		System.out.println(); 
 	}
-	
+
 	public static void main (String [] args) {
 		Flag flag = new Flag(); 
-		color [] colors = {color.red, color.red, color.blue, color.white, color.white, color.red, 
-							color.white, color.blue, color.red, color.blue, color.red}; 
+		
+		//declare some arrays
+		//Case 1, no colors
+		color [] colors = {};
+		//Case 2, only blue
+		color [] colors1 = {color.blue};
+		//Case 3, only red
+		color [] colors2 = {color.red};
+		//Case 4, only white
+		color [] colors3 = {color.white};
+		//Case 5, blue and red
+		color [] colors4 = {color.blue, color.blue, color.blue, color.red, color.blue, color.red};
+		//Case 6, blue and white
+		color [] colors5 = {color.white, color.white, color.white, color.blue, color.blue};
+		//Case 7, white and red
+		color [] colors6 = {color.white, color.red, color.white, color.red, color.white, color.red, color.white};
+		//Case 8, all three colors
+		color [] colors7 = {color.red, color.red, color.blue, color.white, color.white, color.red, 
+				color.white, color.blue, color.red, color.blue, color.red}; 
+		//Case 9, already sorted
+		color [] colors8 = {color.red, color.red, color.white, color.white, color.white, color.blue, color.blue}; 
+	
+		//test each case with both invariants
+		//Case 1
+		flag.InvariantA(colors);
+		System.out.println("Invariant A sort on empty array of colors: \n");
+		flag.printColor(colors);
 		
 		flag.InvariantD(colors);
+		System.out.println("Invariant D sort on empty array of colors: \n");
 		flag.printColor(colors);
+		
+		//Case 2
+		flag.InvariantA(colors1);
+		System.out.println("Invariant A sort on colors1: \n");
+		flag.printColor(colors1);
+
+		flag.InvariantD(colors1);
+		System.out.println("Invariant D sort on colors1: \n");
+		flag.printColor(colors1);
+		
+		//Case 3
+		flag.InvariantA(colors2);
+		System.out.println("Invariant A sort on colors2: \n");
+		flag.printColor(colors2);
+
+		flag.InvariantD(colors2);
+		System.out.println("Invariant D sort on colors2: \n");
+		flag.printColor(colors2);
+		
+		//Case 4
+		flag.InvariantA(colors3);
+		System.out.println("Invariant A sort on colors3: \n");
+		flag.printColor(colors3);
+
+		flag.InvariantD(colors3);
+		System.out.println("Invariant D sort on colors3: \n");
+		flag.printColor(colors3);
+		
+		//Case 5
+		flag.InvariantA(colors4);
+		System.out.println("Invariant A sort on colors4: \n");
+		flag.printColor(colors4);
+
+		flag.InvariantD(colors4);
+		System.out.println("Invariant D sort on colors4: \n");
+		flag.printColor(colors4);
+		
+		//Case 6
+		flag.InvariantA(colors5);
+		System.out.println("Invariant A sort on colors5: \n");
+		flag.printColor(colors5);
+
+		flag.InvariantD(colors5);
+		System.out.println("Invariant D sort on colors5: \n");
+		flag.printColor(colors5);
+		
+		//Case 7
+		flag.InvariantA(colors6);
+		System.out.println("Invariant A sort on colors6: \n");
+		flag.printColor(colors6);
+
+		flag.InvariantD(colors6);
+		System.out.println("Invariant D sort on colors6: \n");
+		flag.printColor(colors6);
+		
+		//Case 8
+		flag.InvariantA(colors7);
+		System.out.println("Invariant A sort on colors7: \n");
+		flag.printColor(colors7);
+
+		flag.InvariantD(colors7);
+		System.out.println("Invariant D sort on colors7: \n");
+		flag.printColor(colors7);
+		
+		//Case 9
+		flag.InvariantA(colors8);
+		System.out.println("Invariant A sort on colors8: \n");
+		flag.printColor(colors8);
+
+		flag.InvariantD(colors8);
+		System.out.println("Invariant D sort on colors8: \n");
+		flag.printColor(colors8);
+		
 	}
 }
--- Partitions.java
+++ Partitions.java
@@ -1,11 +1,21 @@
 package partitions;
 
-import java.util.Arrays;
-
+/**
+ * The Partitions class provides methods to perform partition, selection of the k_th smallest number, 
+ * finding the median and quickSort on an array of integers. 
+ * Instances of Partitions have no fields. 
+ * */
 public class Partitions {
 	
 	public Partitions() {}
-	
+
+	/**
+	 * Print every elements of an array of ints. 
+	 * Pre-conditions: none
+	 * Post-conditions: The array of ints is printed
+	 * @param an array of ints
+	 * @return nothing
+	 * */
 	public void printArray(int [] arr) {
 		int len = arr.length; 
 		for (int i = 0; i < len; i++) {
@@ -13,90 +23,188 @@ public class Partitions {
 		}
 		System.out.println();
 	}
-	
+
+	/**
+	 * Method to perform partition on an array of ints. 
+	 * Pre-conditions: left and right should be within the bounds of the array, left < right
+	 * Post-conditions: the array is partitioned with the first element as the pivot, meaning the pivot
+	 * is in its sorted position and every element before pivot is smaller than pivot, and every element
+	 * after pivot is larger than pivot
+	 * @param an array of ints, 2 indicies of the array
+	 * @return an int which represents the index of the pivot after the partition
+	 * */
 	public int partition(int [] nums, int left, int right) { 
 		int initial = left; 
- 		int pivot = nums[left]; 
- 		left++; 
- 		while (left <= right) {
- 			while (left <= right && nums[left] <= pivot) {
- 				left++; 
- 			}
- 			while (left <= right && nums[right] > pivot) {
- 				right--; 
- 			}
- 			if (left <= right) {
- 				int temp = nums[right]; 
- 	 			nums[right] = nums[left]; 
- 	 			nums[left] = temp; 
- 			}
- 			
- 		}
- 			nums[initial] = nums[right]; 
- 	 		nums[right] = pivot; 
- 	 		return right;
+		int pivot = nums[left]; 
+		left++; 
+		while (left <= right) {
+			while (left <= right && nums[left] <= pivot) {
+				left++; 
+			}
+			while (left <= right && nums[right] > pivot) {
+				right--; 
+			}
+			if (left < right) {
+				int temp = nums[right]; 
+				nums[right] = nums[left]; 
+				nums[left] = temp; 
+			}
+		}
+		nums[initial] = nums[right]; 
+		nums[right] = pivot; 
+		return right;
 	}
-	
+
+	/**
+	 * Select the k_th smallest number in an array
+	 * Pre-conditions: k <= nums.length
+	 * Post-conditions: method must return the k-smallest int in the array
+	 * 					the element is in the input array
+	 * @param nums - an array of ints to be selected from 
+	 * @param k - an int to indicate the k_smallest int we want to select
+	 * @return the k-smallest int in the array
+	 * */
 	public int select (int [] nums, int k) {
 		return selectHelper(nums, k, 0, nums.length - 1); 
 	}
-	
+
+	/**
+	 * A helper method to select the k_th smallest number given a sub-array
+	 * Pre-conditions: k <= nums.length, left and right should be legal indicies in the array, left < right
+	 * Post-conditions: method must return the k-smallest element in the array
+	 * 					the element is in the input array
+	 * @param nums - an array of ints, 
+	 * @param k - an int to indicate the k_smallest int we want to select
+	 * @param left - the starting index of the sub-array (inclusive)
+	 * @param right - the ending index of the sub-array (inclusive)
+	 * @return the k-smallest element in the array
+	 * */
 	public int selectHelper(int [] nums, int k, int left, int right) {
 		int middle = partition(nums, left, right); 
-		if (middle - left == k - 1) return nums[middle]; 
-		if (middle - left >= k) return selectHelper(nums, k, left, middle - 1); 
-		return selectHelper(nums, k - (middle - left) - 1, middle + 1, right); 
+		while (middle != k - 1) {
+			if (middle < k - 1) {
+				middle = partition(nums, middle + 1, right); 
+			}
+			else if (middle > k - 1) {
+				middle = partition(nums, left, middle - 1); 
+			}
+		}
+		return nums[middle]; 
 	}
-	
+
+	/**
+	 * A method to find the median of an array
+	 * Pre-conditions: none
+	 * Post-conditions: return the median of the input array of ints
+	 * 					if the array is of even length, either of the middle values is returned. 
+	 * @param nums - an array of ints
+	 * @return the median value of the array
+	 * */
 	public int median (int [] nums) {
 		int middle = nums.length/2; 
 		return select(nums, middle); 
 	}
-	
+
+	/**
+	 * A wrapper method to perform quick sort on an input array of ints.
+	 * Pre-conditions: n = nums.length
+	 * Post-conditions: the array will be sorted
+	 * @param nums - an array of ints
+	 * @param n - the length of the input array
+	 * @return nothing
+	 * */
 	public void quickSort (int [] nums, int n) {
 		quickSortKernel(nums, 0, n - 1); 
 	}
-	
-	public void quickSortKernel(int [] nums, int start, int end) {
-		if (start < end) {
-			int mid = partition(nums, start, end); 
-			quickSortKernel(nums, start, mid - 1); 
-			quickSortKernel(nums, mid + 1, end); 
+
+	/**
+	 * A helper method to perform quick sort on a sub-array
+	 * Pre-conditions: start and end represent indicies in the array
+	 * Post-conditions: the array will be sorted
+	 * @param nums - an array of ints, 
+	 * @param left - the starting index of the sub-array (inclusive)
+	 * @param right - the ending index of the sub-array (inclusive)
+	 * @return nothing
+	 * */
+	public void quickSortKernel(int [] nums, int left, int right) {
+		if (left < right) {
+			int mid = partition(nums, left, right); 
+			quickSortKernel(nums, left, mid - 1); 
+			quickSortKernel(nums, mid + 1, right); 
 		}
 	}
-	
-	
-	
+
+
 	public static void main (String [] args) {
-		int [] arr = {1, -2, 4, 9, -10, 3, 7}; 
-		int [] arr2 = {1, 2, 3, 4, 5, 6, 7, 8}; 
-		int [] arr3 = {9, 8, 7, 6, 5, 4, 3, 2, 1}; 
 		Partitions obj = new Partitions(); 
 		
+		//declare some arrays
+		//Case 1) odd length array
+		int [] arr = {1, -2, 4, 9, -10, 3, 7}; 
+		//Case 2 and 4) even length array in ascending order 
+		int [] arr2 = {1, 2, 3, 4, 5, 6, 7, 8}; 
+		//Case 5) odd length array in descending order
+		int [] arr3 = {9, 8, 7, 6, 5, 4, 3, 2, 1}; 
 		
-		int select1 = obj.select(arr, 6); 
-		int select2 = obj.select(arr2, 2); 
-		int select3 = obj.select(arr2, 6); 
-		System.out.println(select1);
-		System.out.println();
-		System.out.println(select2);
-		System.out.println();
-		System.out.println(select3);
-		System.out.println();
+		//partition test on sub-array
+		System.out.println("Partition of arr l=3, r=6: \n");
+		System.out.println(obj.partition(arr, 3, 6));
+		System.out.println("Print arr after partition: \n");
 		obj.printArray(arr);
 		System.out.println();
 		
+		System.out.println("Partition of arr3 l=2, r=8: \n");
+		System.out.println(obj.partition(arr3, 2, 8));
+		System.out.println("Print arr3 after partition: \n");
+		obj.printArray(arr3);
+		System.out.println();
+	
+		//testing partition on the entire array: 
+		System.out.println("Partition of arr l=0, r=6: \n");
+		System.out.println(obj.partition(arr, 0, 6));
+		System.out.println("Print arr after partition: \n");
+		obj.printArray(arr);
+		System.out.println();
+		
+		System.out.println("Partition of arr2 l=0, r=5: \n");
+		System.out.println(obj.partition(arr2, 0, 7));
+		System.out.println("Print arr2 after partition: \n");
+		obj.printArray(arr2);
+		System.out.println();
+		
+		//select test
+		int select1 = obj.select(arr, 4); 
+		System.out.println("4th smallest element of arr: \n");
+		System.out.println(select1);
+		System.out.println();
+		
+		//Case 3) k = length of array
+		int select2 = obj.select(arr2, 8); 
+		System.out.println("8th smallest element of arr2: \n");
+		System.out.println(select2);
+		System.out.println();
+		
+		//median test
+		System.out.println("Median of arr: \n");
+		int median = obj.median(arr);
+		System.out.println(median);
+		
+		System.out.println("Median of arr2: \n");
+		int median2 = obj.median(arr2);
+		System.out.println(median2);
+
+		//quick sort test
 		obj.quickSort(arr, arr.length);
 		obj.quickSort(arr2, arr2.length);
 		obj.quickSort(arr3, arr3.length);
+		System.out.println("Print arr after quickSort: \n");
 		obj.printArray(arr);
 		System.out.println();
+		System.out.println("Print arr2 after quickSort: \n");
 		obj.printArray(arr2);
 		System.out.println();
-
+		System.out.println("Print arr3 after quickSort: \n");
 		obj.printArray(arr3);
-		System.out.println();
-
-		
+		System.out.println();	
 	}
 }
